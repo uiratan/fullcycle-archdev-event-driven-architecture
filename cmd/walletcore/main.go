8c360c5e6184cadb35e2d723fa433ed151e4d3ce
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-20 20:36:40 -0300

    webserver

---
+++ cmd/walletcore/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/database"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/event"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/usecase/create_account"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/usecase/create_client"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/usecase/create_transaction"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/web"
+	"github.com/uiratan/fullcycle-archdev-microservices/internal/web/webserver"
+	"github.com/uiratan/fullcycle-archdev-microservices/pkg/events"
+)
+
+func main() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	eventDispatcher := events.NewEventDispatcher()
+	transactionCreatedEvent := event.NewTransactionCreated()
+	// eventDispatcher.Register("TransactionCreated", handler)
+
+	clientDb := database.NewClientDB(db)
+	accountDb := database.NewAccountDB(db)
+	transactionDb := database.NewTransactionDB(db)
+
+	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
+	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+
+	webserver := webserver.NewWebServer(":3000")
+
+	clientHandler := web.NewWebClientHandler(*createClientUseCase)
+	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
+	transactionHandler := web.NewWebTransactionHandler(*createTransactionUseCase)
+
+	webserver.AddHandler("/clients", clientHandler.CreateClient)
+	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
+	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
+
+	webserver.Start()
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/uiratan/fullcycle-archdev-microservices
 go 1.22.5
 
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.6.0
