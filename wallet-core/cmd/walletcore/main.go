eb15cf4a12ff145ec0e74edcfe78eb7b67ea5a63
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-21 15:55:03 -0300

    migrations

--- wallet-core/cmd/walletcore/main.go
+++ wallet-core/cmd/walletcore/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"log"
+	"time"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 	"github.com/uiratan/fullcycle-archdev-microservices/wallet-core/internal/database"
 	"github.com/uiratan/fullcycle-archdev-microservices/wallet-core/internal/event"
 	"github.com/uiratan/fullcycle-archdev-microservices/wallet-core/internal/event/handler"
@@ -20,48 +24,19 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(mysql:3306)/wallet?charset=utf8&parseTime=True&loc=Local")
+	db, err := sql.Open("mysql", "root:root@tcp(mysql-wallet:3306)/wallet?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS accounts (id varchar(255), client_id varchar(255), balance integer, created_at date)")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS transactions (id varchar(255), account_id_from varchar(255), account_id_to varchar(255), amount integer, created_at date)")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("INSERT INTO clients (id, name, email, created_at) VALUES ('d6a3b836-bbd0-11ee-8a24-0242ac120003', 'Uiratan', 'u@u.com', '2020-01-01'), ('d6a3bac8-bbd0-11ee-8a24-0242ac120003', 'Jane', 'janedoe@email.com', '2020-01-02'), ('d6a3bb72-bbd0-11ee-8a24-0242ac120003', 'Alice', 'alice@email.com', '2020-01-03'), ('d6a3bbc1-bbd0-11ee-8a24-0242ac120003', 'Bob', 'bob@email.com', '2020-01-04'), ('d6a3bc01-bbd0-11ee-8a24-0242ac120003', 'Charlie', 'charlie@email.com', '2020-01-05'), ('d6a3bc3e-bbd0-11ee-8a24-0242ac120003', 'David', 'david@email.com', '2020-01-06'), ('d6a3bc81-bbd0-11ee-8a24-0242ac120003', 'Eva', 'eva@email.com', '2020-01-07'), ('d6a3bcb9-bbd0-11ee-8a24-0242ac120003', 'Frank', 'frank@email.com', '2020-01-08'), ('d6a3bd0a-bbd0-11ee-8a24-0242ac120003', 'Grace', 'grace@email.com', '2020-01-09'), ('d6a3bd5c-bbd0-11ee-8a24-0242ac120003', 'Harry', 'harry@email.com', '2020-01-10')")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("INSERT INTO accounts (id, client_id, balance, created_at) VALUES ('d6a4053e-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'John'), 1000, '2020-01-01'), ('d6a40ce9-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Jane'), 1000, '2020-01-02'), ('d6a40f3a-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Alice'), 1000, '2020-01-03'), ('d6a41083-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Bob'), 1000, '2020-01-04'), ('d6a411ce-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Charlie'), 1000, '2020-01-05'), ('d6a412fe-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'David'), 1000, '2020-01-06'), ('d6a41403-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Eva'), 800, '2020-01-07'), ('d6a41504-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Frank'), 1200, '2020-01-08'), ('d6a41604-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Grace'), 1000, '2020-01-09'), ('d6a416ee-bbd0-11ee-8a24-0242ac120003', (SELECT id FROM clients WHERE name = 'Harry'), 1000, '2020-01-10')")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("INSERT INTO transactions (id, account_id_from, account_id_to, amount, created_at) VALUES (UUID(), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'John')), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Jane')), 100, '2020-01-01'), (UUID(), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Alice')), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Bob')), 100, '2020-01-02'), (UUID(), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Charlie')), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'David')), 100, '2020-01-03'), (UUID(), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Eva')), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Frank')), 100, '2020-01-04'), (UUID(), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Grace')), (SELECT id FROM accounts WHERE client_id = (SELECT id FROM clients WHERE name = 'Harry')), 100, '2020-01-05')")
-	if err != nil {
-		panic(err)
-	}
+	createTablesDb(db)
+	populateDb(db)
 
 	configMap := ckafka.ConfigMap{
 		"bootstrap.servers": "kafka:29092",
 		"group.id":          "wallet",
 	}
 	kafkaProducer := kafka.NewKafkaProducer(&configMap)
-
-	// eventDispatcher := events.NewEventDispatcher()
-	// eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
-	// eventDispatcher.Register("BalanceUpdated", handler.NewUpdateBalanceKafkaHandler(kafkaProducer))
-	// transactionCreatedEvent := event.NewTransactionCreated()
-	// balanceUpdatedEvent := event.NewBalanceUpdated()
 
 	eventDispatcher := events.NewEventDispatcher()
 	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
@@ -98,4 +73,62 @@ func main() {
 	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
 	webserver.Start()
+}
+
+func createTablesDb(db *sql.DB) {
+	fmt.Println("Creating tables...")
+	fmt.Println("=============================")
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS clients (id varchar(255), name varchar(255), email varchar(255), created_at date)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DB: TABLE clients CREATED")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS accounts (id varchar(255), client_id varchar(255), balance integer, created_at date)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DB: TABLE accounts CREATED")
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS transactions (id varchar(255), account_id_from varchar(255), account_id_to varchar(255), amount integer, created_at date)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DB: TABLE transactions CREATED")
+}
+
+func populateDb(db *sql.DB) {
+	fmt.Println("Populating DB...")
+	var previousAccountID string
+	for i := 1; i <= 5; i++ {
+		fmt.Println("=============================")
+		clientID := uuid.NewString()
+		_, err := db.Exec(`INSERT INTO clients (id, name, email, created_at) VALUES (?, ?, ?, ?)`,
+			clientID, fmt.Sprintf("Client %d", i), fmt.Sprintf("client%d@example.com", i), time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Client inserted: ? ? ? ?", clientID, fmt.Sprintf("Client %d", i), fmt.Sprintf("client%d@example.com", i), time.Now())
+
+		accountID := uuid.NewString()
+		_, err = db.Exec(`INSERT INTO accounts (id, client_id, balance, created_at) VALUES (?, ?, ?, ?)`,
+			accountID, clientID, i*1000, time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Account inserted: ? ? ? ?", accountID, clientID, i*1000, time.Now())
+
+		if previousAccountID != "" {
+			transactionID := uuid.NewString()
+			_, err = db.Exec(`INSERT INTO transactions (id, account_id_from, account_id_to, amount, created_at) VALUES (?, ?, ?, ?, ?)`,
+				transactionID, previousAccountID, accountID, 100*i, time.Now())
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Transaction inserted: ? ? ? ?", transactionID, previousAccountID, accountID, 100*i, time.Now())
+		}
+
+		previousAccountID = accountID
+	}
+	fmt.Println("Records inserted successfully")
 }
