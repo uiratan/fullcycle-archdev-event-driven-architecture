ca172f451ee9f00784c915320a1df48bed727f29
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-21 16:25:23 -0300

    balances gateway interface

---
+++ balances/internal/gateway/balance.go
@@ -0,0 +1,10 @@
+package gateway
+
+import "github.com/uiratan/fullcycle-archdev-event-driven-architecture-utils/balances/internal/entity"
+
+type BalanceGateway interface {
+	Save(balance *entity.Balance) error
+	FindByID(id string) (*entity.Balance, error)
+	FindByAccountID(accountID string) (*entity.Balance, error)
+	Update(balance *entity.Balance) error
+}
