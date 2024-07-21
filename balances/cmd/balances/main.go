24e20b9cfb946c8db88312dd3176d284bf88c852
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-21 18:27:09 -0300

    cmd goroutines and sql fix

--- balances/cmd/balances/main.go
+++ balances/cmd/balances/main.go
@@ -32,27 +32,21 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS balances (id varchar(255), account_id varchar(255), balance integer, created_at timestamp)")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("INSERT INTO balances (id, account_id, balance, created_at) VALUES ('b8bbd006-0318-48ba-b7a4-30dc5920f4c2', 'd6a41403-bbd0-11ee-8a24-0242ac120003', 850, '2024-01-27 17:13:48'), ('ab37cbe9-b212-446b-a7ca-3dc8117281d3', 'd6a41504-bbd0-11ee-8a24-0242ac120003', 1150, '2024-01-27 17:13:48'), ('d10d6ec7-92ed-4387-a689-422fe1559a30', 'd6a41403-bbd0-11ee-8a24-0242ac120003', 800, '2024-01-27 17:13:55'), ('5af44c65-fc2f-4cfc-9b8b-5a908bda18b1', 'd6a41504-bbd0-11ee-8a24-0242ac120003', 1200, '2024-01-27 17:13:55')")
-	if err != nil {
-		panic(err)
-	}
+	createTablesDb(db)
+	populateDb(db)
 
 	balanceDb := database.NewBalanceDB(db)
 
 	createBalanceUseCase := create_balance.NewCreateBalanceUseCase(balanceDb)
 	findAccountUseCase := find_account.NewFindAccountUseCase(balanceDb)
 
-	// go func() {
-	webserver := webserver.NewWebServer(":3003")
-	accountHandler := web.NewWebBalanceHandler(*findAccountUseCase)
-	webserver.AddHandler("/accounts/{account_id}", accountHandler.FindAccount)
-	fmt.Println("Server running at port 3003")
-	webserver.Start()
-	// }()
+	go func() {
+		webserver := webserver.NewWebServer(":3003")
+		accountHandler := web.NewWebBalanceHandler(*findAccountUseCase)
+		webserver.AddHandler("/balances/{account_id}", accountHandler.FindAccount)
+		fmt.Println("Server running at port 3003")
+		webserver.Start()
+	}()
 
 	configMap := &kafka.ConfigMap{
 		"bootstrap.servers": "kafka:29092",
@@ -95,4 +89,23 @@ func main() {
 		c.CommitMessage(msg)
 	}
 
+}
+
+func createTablesDb(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS balances (id varchar(255), account_id varchar(255), balance integer, created_at timestamp)")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func populateDb(db *sql.DB) {
+	_, err := db.Exec(`INSERT INTO balances (id, account_id, balance, created_at) VALUES 
+										('3446e7a4-2a02-4c16-9cdf-62c0c31adfe4', '6b76d113-257a-4942-93fd-d97c42491dac', 1000, '2024-07-21'),
+										('f3c897f4-2593-461e-a4a3-dd84b075dca0', 'cc457010-c5fe-4e94-9492-3e0aa0554625', 2000, '2024-07-21'),
+										('f762d214-e880-4b73-bea5-317c2b454b4e', '2b42674d-10ce-49fb-a976-593d05198a42', 3000, '2024-07-21'),
+										('61a2a5f4-e494-4f99-849b-5df122cd956d', 'b41337b8-13f5-413b-8112-3d221d6b15c1', 4000, '2024-07-21'),
+										('860ce9fc-e243-4568-9ad7-03922fefc67d', '372b5874-252a-48ac-80d6-d9c4c2f10114', 5000, '2024-07-21')`)
+	if err != nil {
+		panic(err)
+	}
 }
